6609fafb8b8b8d53f37294bbeff846757be31217
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-07-19 16:32:19 +0200

    Update

--- PSWebToolbox.Tests.ps1
+++ PSWebToolbox.Tests.ps1
@@ -1,23 +1,23 @@
 $PSVersionTable.PSVersion
 
 $ModuleName = (Get-ChildItem $PSScriptRoot\*.psd1).BaseName
+#$ModuleVersion = (Get-Content -Raw $PSScriptRoot\*.psd1)  | Invoke-Expression | ForEach-Object ModuleVersion
+
+#$Dest = "Builds\$ModuleName-{0}-{1}.zip" -f $ModuleVersion, (Get-Date).ToString("yyyyMMddHHmmss")
+#Compress-Archive -Path . -DestinationPath .\$dest
 
 $Pester = (Get-Module -ListAvailable pester)
 if ($null -eq $Pester -or ($Pester[0].Version.Major -le 4 -and $Pester[0].Version.Minor -lt 4)) {
     Write-Warning "$ModuleName - Downloading Pester from PSGallery"
     Install-Module -Name Pester -Repository PSGallery -Force -SkipPublisherCheck -Scope CurrentUser
 }
-
-$RequiredModules = (Get-Content -Raw $PSScriptRoot\*.psd1)  | Invoke-Expression | ForEach-Object RequiredModules
-foreach ($Module in $RequiredModules) {
-    $ModuleFound = Get-Module -ListAvailable $Module
-    if ($null -eq $ModuleFound) {
-        Write-Warning "$ModuleName - Downloading $Module from PSGallery"
-        Install-Module -Name $Module -Repository PSGallery -Force -Scope CurrentUser
-    }
+if ($null -eq (Get-Module -ListAvailable PSSharedGoods)) {
+    Write-Warning "$ModuleName - Downloading PSSharedGoods from PSGallery"
+    Install-Module -Name PSSharedGoods -Repository PSGallery -Force -Scope CurrentUser
 }
+import-module pssharedgoods -force
 
-$result = Invoke-Pester -Script @{ Path = "$($PSScriptRoot)\Tests"; } -EnableExit
+$result = Invoke-Pester -Script $PSScriptRoot\Tests -EnableExit
 
 if ($result.FailedCount -gt 0) {
     throw "$($result.FailedCount) tests failed."
--- Public/Get-AtomFeed.ps1
+++
@@ -1,3 +0,0 @@
-function Get-AtomFeed {
-
-}
